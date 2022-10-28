ea83d600a9cd4c531d45f8ac585b3b7a46acf8ef
Dmytro <ihnatenko.dev@gmail.com>
2022-10-28 19:17:22 +0300

    Create <WarStatistic/>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import { Header } from './components/Header/Header';
+import WarStatistic from './components/WarStatistic/WarStatistic';
 
 const App: FC = () => {
 	return (
 		<main>
 			<Header />
+			<WarStatistic />
 		</main>
 	);
 };
---
+++ src/components/WarStatistic/WarStatistic.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react';
+import s from './WarStatistic.module.scss';
+import cn from 'classnames';
+
+const daysOfWar = '247';
+
+const statisticData = [
+	{
+		id: 1,
+		name: 'особовий склад',
+		count: '~69700',
+		icon: '/icons/statistic/helmet.svg',
+	},
+	{ id: 2, name: 'танки', count: '2640', icon: '/icons/statistic/tank.svg' },
+	{
+		id: 3,
+		name: 'артсистеми',
+		count: '4578',
+		icon: '/icons/statistic/art.svg',
+	},
+	{ id: 4, name: 'лiтаки', count: '272', icon: '/icons/statistic/fly.svg' },
+	{ id: 5, name: 'РЗСВ', count: '379', icon: '/icons/statistic/missile.svg' },
+	{
+		id: 6,
+		name: 'гелiкоптери',
+		count: '251',
+		icon: '/icons/statistic/helicopter.svg',
+	},
+	{ id: 7, name: 'ББМ', count: '5378', icon: '/icons/statistic/bbm.svg' },
+];
+
+const WarStatistic: FC = () => {
+	return (
+		<section className={cn(s.stats, 'container')}>
+			<div className={s.stats__days}>{daysOfWar} день вiйни:</div>
+			<div className={s.stats__count}>
+				{statisticData.map((stat) => (
+					<div key={stat.id} className={s.stats__count_item}>
+						<img src={stat.icon} alt={stat.name} />
+						<p>
+							{stat.name} <span>{stat.count}</span>
+						</p>
+					</div>
+				))}
+			</div>
+		</section>
+	);
+};
+
+export default WarStatistic;
