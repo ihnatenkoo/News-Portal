b0c87fe8979f3a392856438653ad4790587f1b65
Dmytro <ihnatenko.dev@gmail.com>
2023-02-19 16:33:34 +0200

    Header BottomBar add Swiper

--- src/components/Header/BottomBar/BottomBar.tsx
+++ src/components/Header/BottomBar/BottomBar.tsx
@@ -1,25 +1,43 @@
 import { FC } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import s from './BottomBar.module.scss';
+
+const categories = [
+	'війна',
+	'новини',
+	'політика',
+	'економіка',
+	'суспільство',
+	'погляди',
+	'світ',
+];
 
 const BottomBar: FC = () => {
 	return (
 		<nav className={s.nav}>
-			<div className={s.nav__articles}>
-				<img src="/icons/fair.svg" alt="fair" className={s.nav__articles_hot} />
-
-				<div className={s.nav__articles_links}>
-					<a className={s.active}>війна</a>
-					<a>новини</a>
-					<a>політика</a>
-					<a>економіка</a>
-					<a>суспільство</a>
-					<a>погляди</a>
-					<a>світ</a>
-				</div>
-			</div>
+			<Swiper
+				slidesPerView={'auto'}
+				breakpoints={{
+					320: { spaceBetween: 25 },
+					1024: {
+						spaceBetween: 50,
+					},
+					1300: {
+						spaceBetween: 100,
+					},
+				}}
+				style={{ width: '100%' }}
+			>
+				{categories.map((m) => (
+					<SwiperSlide key={m} className={s.nav__links}>
+						{m === 'війна' && <img src="/icons/fair.svg" alt="fair" />}
+						<a>{m}</a>
+					</SwiperSlide>
+				))}
+			</Swiper>
 
 			<div className={s.nav__live}>
-				<img src="/icons/live-dot.svg" alt="" />
+				<img src="/icons/live-dot.svg" alt="live" />
 				<span>LIVE</span>
 			</div>
 		</nav>
